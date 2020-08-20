4edd9b4776fcb8980aa1b62e97c9030e4e563fe5
likui911 <likui911@hotmail.com>
2020-08-20 11:49:26 +0800

    react jsx

---
+++ react_hello/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+      <button onClick={() => this.setState({ liked: true }) }>
+        Like
+      </button>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LikeButton />
+      </header>
+    </div>
+  );
+}
+
+export default App;
