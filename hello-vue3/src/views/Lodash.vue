b31c3d40ee12bbedf94e9f031b7f0d63955a926e
likui911 <likui911@hotmail.com>
2020-12-28 11:49:28 +0800

    lodash

---
+++ hello-vue3/src/views/Lodash.vue
@@ -0,0 +1,29 @@
+<template>
+    <view>{{ count }}</view>
+    <button @click="debouncedClick">click me</button>
+</template>
+
+<script>
+import _ from "lodash";
+export default {
+    created() {
+        this.debouncedClick = _.debounce(this.click, 500);
+    },
+    unmounted() {
+        this.debouncedClick.cancel();
+    },
+    data() {
+        return {
+            count: 1,
+        };
+    },
+    methods: {
+        click() {
+            this.count++;
+        },
+    },
+};
+</script>
+
+<style>
+</style>
